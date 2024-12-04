e5393e65f952620710d96440f027fb59e5527a19
David <david.padillac15@gmail.com>
2024-12-03 21:35:33 -0600

    agregando solucion optimizada

--- Interview_preparation_kit/Warm_up_challenges/Repeated_Strings.js
+++ Interview_preparation_kit/Warm_up_challenges/Repeated_Strings.js
@@ -20,3 +20,22 @@ function repeatedString(s, n) {
         }
    
 }
+
+//Solucion optimizada
+function repeatedString(s, n) {
+    const len = s.length;
+    const rem = n % len;
+    let totalA = 0;
+    let remA = 0;
+
+    // Contamos cuántas veces aparece 'a' en una repetición completa
+    for(let i = 0; i < len; i++) {
+        if(s[i] == 'a') {
+            totalA++;  // Cuenta 'a' en la cadena completa
+            if(i < rem) remA++;  // Cuenta 'a' en el "rem" restante
+        }
+    }
+
+    // Calcula el número total de 'a's en las repeticiones completas y el resto
+    return Math.floor(n / len) * totalA + remA;
+}
